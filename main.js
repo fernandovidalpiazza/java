d82249d215564d05cb3eb903b326b2db14871154
fernandovidalpiazza <licvidalfernando@gmail.com>
2023-03-23 13:37:07 -0300

    mejorebucle

--- main.js
+++ main.js
@@ -1,92 +1,88 @@
 
-/*
-let ID1
-do{
- ID1 =prompt ("ingrese el id de su pocima faborita del 1 al 6 o s para finalizar la comrpa" )
-  if (ID1 <= 6) {
-   
-    console.log ("ud. a comprado la posima" )
-   
+let continuar = true
+while (continuar){
+let ID = prompt ("ingrese el id de su pócima favorita del 1 al 6 " )
 
-  }else{
-  
-    console.log ("ingrese un numero id del 1 al 6 o s para salir o c para continuar")
-  
+
+
+
+
+
+function resultadodonacion (cantidad, donacion, ID){
+  let resultado  =  (cantidad * donacion) 
+    alert  ("gracias por donar $ "  + resultado + " Ud. va a recibir la pocima N°" + ID )
+    
 }
 
-}while (ID1 != "s")   
-console.log ("finalizo la compra")
-alert ("ud a finalizado sus compras")  
-
-function ingrese valor de donacion
-   
-  */
-
-let ID = prompt ("ingrese el id de su pocima faborita del 1 al 6 " )
 
 
 
 
-
-function resultadodonacion(cantidad, donacion){
-  let resultado = cantidad * donacion
-  alert  ("gracias por donar $ "  + resultado  )
-
-}
-
-  do {
 switch (ID) {
   
     case "1":
        alert ("Ud. eligió la pócima de la felicidad ")
-       cantidad = prompt ("Ingrese la cantidad de pócimas de la felicidad deseadas")
+       do{
+       cantidad = parseInt(prompt ("Ingrese la cantidad de pócimas de la felicidad deseadas"))
        donacion = prompt ("ingrese el monto a donar por pocimas")
-       resultadodonacion (cantidad,donacion)
+      }while(isNaN(cantidad, donacion))
+       resultadodonacion (cantidad,donacion, ID)
       break
 
     case "2":
        alert ("Ud. eligió la pócima dinero por siempre")
-       cantidad = prompt ("Ingrese la cantidad de pócimas dinero por siempre ")
+       do{
+       cantidad = parseInt(prompt ("Ingrese la cantidad de pócimas dinero por siempre "))
        donacion = prompt ("Ingrese el monto a donar por pócimas")
-       resultadodonacion (cantidad,donacion)
+      }while(isNaN(cantidad, donacion))
+       resultadodonacion (cantidad,donacion, ID)
       break
 
     case "3":
        alert ("Ud. eligió la pócima dinero por siempre")
-        cantidad = prompt ("ingrese al cantindad de pócimas dinero por siempre")
+       do{
+        cantidad = parseInt (prompt ("ingrese al cantindad de pócimas dinero por siempre"))
         donacion = prompt ("ingrese el monto a donar por pócimas")
-        resultadodonacion (cantidad,donacion)
+        }while(isNaN(cantidad, donacion))
+        resultadodonacion (cantidad,donacion, ID)
         break
 
     case "4":
         alert ("Ud. eligió la pócima de belleza por siempre")
-        cantidad = prompt ("ingrese al cantindad de pocimas de belleza por siempre")
+        do{
+        cantidad = parseInt (prompt ("ingrese al cantindad de pocimas de belleza por siempre"))
         donacion = prompt ("ingrese el monto a donar por pocimas")
-        resultadodonacion (cantidad,donacion)
+        }while(isNaN(cantidad, donacion))
+        resultadodonacion (cantidad,donacion, ID)
         break
 
     case "5":
         alert ("Ud. eligio la pócima buena suerte")
-        cantidad = prompt ("ingrese al cantindad de pocimas buena suerte")
+        do{
+        cantidad = parseInt (prompt ("ingrese al cantindad de pocimas buena suerte"))
         donacion = prompt ("ingrese el monto a donar por pócimas")
-        resultadodonacion (cantidad,donacion)
+        }while(isNaN(cantidad, donacion))
+        resultadodonacion (cantidad,donacion, ID)
         break
     case "6":
         alert ("Ud. eligio la pócima abre nuevos caminos")
-        cantidad = prompt ("ingrese al cantindad de pocimas abre caminos")
+        do{
+        cantidad = parseInt (prompt ("ingrese al cantindad de pocimas abre caminos"))
         donacion = prompt ("ingrese el monto a donar por pócimas")
-        resultadodonacion (cantidad,donacion)
+       }while(isNaN(cantidad, donacion))
+        resultadodonacion (cantidad,donacion, ID)
         break
     default:
-      alert ("Ingrese una opcion valida del 1 al 6 o s para salir de las donaciones")
+      alert ("Ingrese una opcion valida del 1 al 6 o cancele para salir")
       break
    
-
-
 }
-ID = prompt ("ingrese el id de su pocima faborita del 1 al 6 o s para finalizar la compra")
-}while (ID != "s")
-    alert ("ud a finalizado su compra")
+continuar=confirm("queres seguir obteniedo pocimas")
+}
+
+
+
+    
 
 
 
@@ -94,14 +90,6 @@ ID = prompt ("ingrese el id de su pocima faborita del 1 al 6 o s para finalizar 
  
 
 
-/*
-const ID2 =prompt ("ingrese el id de su pocima faborita en caso que no quiera comprar mas ingrese l")
-const ID3 =prompt ("ingrese el id de su pocima faborita en caso que no quiera comprar mas ingrese l")
-const ID4 =prompt ("ingrese el id de su pocima faborita en caso que no quiera comprar mas ingrese l")
-const ID5 =prompt ("ingrese el id de su pocima faborita en caso que no quiera comprar mas ingrese l")
-const ID6 =prompt ("ingrese el id de su pocima faborita en caso que no quiera comprar mas ingrese l")
-const ID7 =prompt ("ingrese el id de su pocima faborita en caso que no quiera comprar mas ingrese l")
-*/
 
 
 
